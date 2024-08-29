1c67284e20a2a0c78037978d9e73a7822a71d601
Juan Romero <mateoromero0910@gmail.com>
2024-08-29 14:13:19 -0500

    fix(UserRepository): The id data type was corrected.

---
+++ src/main/java/com/example/projectpulse/dtos/project/write/ProjectCreateDto.java
@@ -0,0 +1,2 @@
+package com.example.projectpulse.dtos.project.write;public class ProjectCreateDto {
+}
---
+++ src/main/java/com/example/projectpulse/repositories/project/ProjectRepository.java
@@ -0,0 +1,2 @@
+package com.example.projectpulse.repositories.project;public class ProjectRepository {
+}
